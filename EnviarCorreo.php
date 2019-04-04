55b180ba6795a43f4b80e9bd6e89251985139b69
Anderson Montes <49078542+COVEP@users.noreply.github.com>
2019-04-04 16:28:35 -0500

    Create EnviarCorreo.php

---
+++ EnviarCorreo.php
@@ -0,0 +1,7 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
