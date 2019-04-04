8b45486c7202aee9f4a37891d9c6bb5c3e8092c5
Anderson Montes <49078542+COVEP@users.noreply.github.com>
2019-04-04 18:58:11 -0500

    Update EnviarCorreo.php

--- EnviarCorreo.php
+++ EnviarCorreo.php
@@ -15,7 +15,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->SMTPDebug = 0;                                       // Enable verbose debug output
     $mail->isSMTP();                                            // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
