d001b3fe1f9f1d5ea38ff2692f5a7fa88415729d
Anderson Montes <49078542+COVEP@users.noreply.github.com>
2019-04-05 17:09:18 -0500

    Update EnviarCorreo.php

--- EnviarCorreo.php
+++ EnviarCorreo.php
@@ -21,7 +21,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'servercovep@gmail.com';                // SMTP username
-    $mail->Password   = 'covepserver2019';                      // SMTP password
+    $mail->Password   = 'contraseña';                      // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
